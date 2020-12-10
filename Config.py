d95a9858bcb3e07373bbadec7b36f049352fef83
Lorenzo Demiri <lorenzo.demiri96@gmail.com>
2020-12-10 16:54:57 +0000

    Remove API keys

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@ TELEGRAM_SEND_MESSAGE_URL = 'https://api.telegram.org/bot{}/deleteWebhook'.forma
 TELEGRAM_CHANNELL_ID = 'Channel Id' #Telegram Channel Id
 LINK_TABLE_ORDERS = 'https://docs.google.com/spreadsheets/d/1ar1NvJlUar5-m5aYmvS-IwJxOrTzmiDGUUMdRi98MJY/edit?usp=sharing'
 TOKEN_ADMINBOT = 'TOKEN ADMIN BOT' #token admin bot
-TELEGRAM_CHANNELL_ID_PUBLIC = '-1001369590564'
+TELEGRAM_CHANNELL_ID_PUBLIC = 'CHANNEL ID'
 URL_STORE_BOT = 'http://t.me/MubbyLabBot'
 ADMIN_PASSWORD = 'Password'
 INSTAGRAM_USERNAME = "mubbylab"
