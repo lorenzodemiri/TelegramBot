81f0c1084d1d8500ab1c2c1c4e6a28407e97970b
Lorenzo Demiri <lorenzo.demiri96@gmail.com>
2020-12-10 16:29:56 +0000

    commit on master

--- Config.py
+++ Config.py
@@ -2,12 +2,12 @@ TOKEN = 'INSERT PASSWORD HERE'
 NGROK_URL = 'https://e8bd2010.ngrok.io'
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
-TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
+TELEGRAM_INIT_WEBHOOK_UL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
 TELEGRAM_DELETE_WEBHOOK_URL = '{}/setWebhook?url='.format(BASE_TELEGRAM_URL)
 TELEGRAM_SEND_MESSAGE_URL = 'https://api.telegram.org/bot{}/deleteWebhook'.format(TOKEN)
 TELEGRAM_CHANNELL_ID = 'Channel Id' #Telegram Channel Id
 LINK_TABLE_ORDERS = 'https://docs.google.com/spreadsheets/d/1ar1NvJlUar5-m5aYmvS-IwJxOrTzmiDGUUMdRi98MJY/edit?usp=sharing'
-TOKEN_ADMINBOT = '1049266407:AAGlAiS64UdAXH2uGdiZkrWWoyrEdjYwAfc' #token admin bot
+TOKEN_ADMINBOT = 'TOKEN ADMIN BOT' #token admin bot
 TELEGRAM_CHANNELL_ID_PUBLIC = '-1001369590564'
 URL_STORE_BOT = 'http://t.me/MubbyLabBot'
 ADMIN_PASSWORD = 'Password'
