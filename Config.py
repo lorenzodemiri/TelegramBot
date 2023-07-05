66afcd56f2c27ea6956beb399987d25ad5ebf47e
Lorenzo Demiri <lorenzo.demiri96@gmail.com>
2023-07-05 08:43:47 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,7 +6,7 @@ TELEGRAM_INIT_WEBHOOK_UL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCA
 TELEGRAM_DELETE_WEBHOOK_URL = '{}/setWebhook?url='.format(BASE_TELEGRAM_URL)
 TELEGRAM_SEND_MESSAGE_URL = 'https://api.telegram.org/bot{}/deleteWebhook'.format(TOKEN)
 TELEGRAM_CHANNELL_ID = 'Channel Id' #Telegram Channel Id
-LINK_TABLE_ORDERS = 'https://docs.google.com/spreadsheets/d/1ar1NvJlUar5-m5aYmvS-IwJxOrTzmiDGUUMdRi98MJY/edit?usp=sharing'
+LINK_TABLE_ORDERS = 'https://docs.google.com/spreadsheets/d/<ID_TABLE>'
 TOKEN_ADMINBOT = 'TOKEN ADMIN BOT' #token admin bot
 TELEGRAM_CHANNELL_ID_PUBLIC = 'CHANNEL ID'
 URL_STORE_BOT = 'http://t.me/MubbyLabBot'
